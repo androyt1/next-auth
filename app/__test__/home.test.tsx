cd5b876d5e9dd6293021ad4c9f3709e741f6992b
Andrew Aghoghovwia <androyt1@gmail.com>
2024-06-18 16:09:16 +0100

    Updated test setup and tested home page

--- app/__test__/home.test.tsx
+++ app/__test__/home.test.tsx
@@ -10,4 +10,18 @@ describe("Home page", () => {
             screen.getByRole("heading", { level: 1, name: /Welcome to our platform/i })
         ).toBeDefined();
     });
+    it("render sub heading", () => {
+        render(<Home />);
+        const subheading = screen.getByText(
+            /The all-in-one platform for building, deploying, and managing modern web apps./i
+        );
+        expect(subheading).toBeInTheDocument();
+    });
+
+    it("Should render signup button", () => {
+        render(<Home />);
+        const signupButton = screen.getByRole("link", { name: /sign up/i });
+        expect(signupButton).toBeInTheDocument();
+        expect(signupButton).toHaveAttribute("href", "/register");
+    });
 });
