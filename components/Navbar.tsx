d1dde4a64f949c21a7f57b459c710d321d66a468
Andrew Aghoghovwia <androyt1@gmail.com>
2024-06-18 14:32:29 +0100

    changed border style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Navbar = () => {
     return (
-        <nav className='container w-full h-16 bg-primary border-b-secondary border-b text-foreground flex justify-between items-center'>
+        <nav className='container w-full h-16 bg-primary border-b-secondary/20 border-b text-foreground flex justify-between items-center'>
             <div className='text-primary-foreground'>
                 <Link href='/' className='text-xl text-gray-400'>
                     Authentication
