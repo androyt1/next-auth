263abe753db0953d1b0191966241dda835235658
Andrew Aghoghovwia <androyt1@gmail.com>
2024-06-18 16:09:51 +0100

    updated testing set up

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
     test: {
+        setupFiles: "./setupTests.ts", // Path to your setup file
+        globals: true,
         environment: "jsdom",
     },
 });
